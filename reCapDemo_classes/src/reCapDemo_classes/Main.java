737f776939e630b9e55fea9af557630c014cd4e2
Abdullah Gul <abdullahgul13@outlook.com>
2022-09-28 17:23:38 +0300

    Java middle examples

---
+++ reCapDemo_classes/src/reCapDemo_classes/Main.java
@@ -0,0 +1,11 @@
+package reCapDemo_classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(2, 4);
+		System.out.println(sonuc);
+	}
+
+}
